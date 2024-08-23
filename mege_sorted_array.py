7749caf30ce86de592b579b658589b63c1834b00
Robel Ermiyas <Robelermiyas7@gmail.com>
2024-08-23 07:01:20 -0700

    Update mege_sorted_array.py

--- mege_sorted_array.py
+++ mege_sorted_array.py
@@ -8,7 +8,7 @@
 # Example 1:
 # Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # Output: [1,2,2,3,5,6]
-# time complexity = O(n)
+# time complexity = O(m + n)
 # space complexity = O(1)
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
